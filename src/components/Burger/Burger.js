4cb48d36f6660f1cfe7cb8d695b1fee559ce85c5
xinwangicf <50649@icf.com>
2020-02-26 09:52:27 -0500

    to 165, added dynamic burger control and burger builder.

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,13 +3,24 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 import './Burger.css';
 
 const burger = (props) => {
+    let transformedIngredients = Object.keys(props.ingredients).map(igKey => {
+        return [...Array(props.ingredients[igKey])].map((_, i) => {
+            return <BurgerIngredient key={igKey + i} type={igKey} />
+        });
+    })
+    .reduce((arr, el)=>{
+        return arr.concat(el)
+    },[])
+    
+    // console.log(transformedIngredients)
+
+    if(transformedIngredients.length==0) {
+        transformedIngredients=<p>Please start adding ingredients!</p>
+    }
     return(
         <div className="Burger">
             <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="meat" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="bacon" />
+            {transformedIngredients}
             <BurgerIngredient type="bread-bottom" />
         </div>
     );
